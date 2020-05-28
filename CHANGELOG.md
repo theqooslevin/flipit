b29520009791f933467170d4e4d70203a9c521e0
David <david@theqoos.com>
2020-05-28 11:17:11 +0900

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+## [0.0.1] - 28 May 2020 (KST).
+
+* released the initial release version.
---
+++ test/flipit_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flipit/flipit.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final _flipit = FlipitListView();
+    expect(() => _flipit == null, throwsNoSuchMethodError);
+  });
+}
